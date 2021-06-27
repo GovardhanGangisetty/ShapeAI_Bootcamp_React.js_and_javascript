2de4e1259a39fa3c35be89b37129b87c77daa0d3
GovardhanGangisetty <86421809+GovardhanGangisetty@users.noreply.github.com>
2021-06-27 18:45:59 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>ShapeAI JS AND REACT.JS </h1>
+      <p>A Web development </p>
+    </div>
+    
+  );
+}
+
+export default Info;
